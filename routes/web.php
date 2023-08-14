8b4560f6ba0e13df6f2090fbe0520daefb58d61c
Santiago Marulanda M <santiagomm1997@gmail.com>
2023-08-15 01:34:28 +0200

    Installing Voyager package

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->to('/admin');
+});
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
 });
